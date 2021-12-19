f963480923674e3c2cd6f058cdfd1b30279ef8ba
oscarhancock99 <hancock.oscar@gmail.com>
2021-12-19 17:09:59 +0000

    Added a card into the show page that displays the name address and cuisine

--- src/pages/restaurants/Show.js
+++ src/pages/restaurants/Show.js
@@ -3,6 +3,13 @@ import { useParams, Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Button } from '@mui/material';
+import Card from '@mui/material/Card';
+import Container from '@mui/material/Container'
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+import Typography from '@mui/material/Typography';
 
 
 const Show = () => {
@@ -48,13 +55,36 @@ const Show = () => {
     return (
       <div>
         <h2>This is the restaurants show page: {_id}</h2>
-
-        <p><b>Name: </b> {restaurant.name} </p>
-        <p><b>Address: </b> {restaurant.address.street} </p>
-        <p><b>Building: </b> {restaurant.address.building} </p>
-        <p><b>Cuisine: </b> {restaurant.cuisine} </p>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        // alt="green iguana"
+      />
+      <CardContent>
+        {/* <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography> */}
+        <Typography gutterBottom variant="h5"><p><b>Name: </b> {restaurant.name} </p>
+        </Typography>
+        <Typography variant="h6" color="text.secondary"><p><b>Address: </b> {restaurant.address.street} </p>
+        </Typography>
+        <Typography variant="h6" color="text.secondary"><p><b>Building: </b> {restaurant.address.building} </p>
+        </Typography>
+        <Typography variant="h6" color="text.secondary"><p><b>Cuisine: </b> {restaurant.cuisine} </p>
+        </Typography>
         <Link to="edit" >Edit</Link>
         <Button onClick={() => Delete(restaurant._id)} variant="contained" color="error" >Delete</Button>
+      </CardContent>
+    
+    </Card>
+
+        
       </div>
     )
   }
